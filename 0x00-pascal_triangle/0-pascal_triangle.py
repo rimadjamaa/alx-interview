86bbd598770226185ffb060a462df0bb141f2c5e
rimadjamaa <djamaarima7@gmail.com>
2024-10-05 20:36:32 +0100

    task upload

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+"""
+0-main
+"""
+
+def pascal_triangle(n):
+ if (n <= 0):
+  return []
+ 
+ triangle =[[1]] #first row is always 1 
+
+ for i in range(1,n) :
+  
+  row = [1] # start each row with 1 
+  perv_row = triangle[i-1]
+
+  for j in range(1,i):
+   row.append(perv_row[j-1]+perv_row[j])
+
+  row.append(1)
+  triangle.append(row)
+
+ return triangle
